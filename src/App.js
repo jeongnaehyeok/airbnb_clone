11e88888d3d49b99030a6f23c2d966a462de1a55
jeongnaehyeok <cnh0301@gmail.com>
2020-10-28 16:39:24 +0900

    feat: Fixedbar 추가

--- src/App.js
+++ src/App.js
@@ -4,15 +4,19 @@ import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
 import "./App.scss";
 import "./scss/reset.scss";
 
+import Fixedbar from "./components/Fixedbar";
+
 import Navbar from "./components/Navbar";
 import Home from "./page/Home";
 
 const App = () => {
   return (
     <BrowserRouter>
+      <Route path="/" exact component={Fixedbar} />
       <Navbar />
       <Switch>
         <Route path="/" exact component={Home} />
+        <Route path="/test" exact component={Navbar} />
         <Redirect path="*" to="/" />
       </Switch>
     </BrowserRouter>
