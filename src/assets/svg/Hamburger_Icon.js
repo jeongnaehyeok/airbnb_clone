c1ef95355e8170d160a4979e2613940e3a53e557
jeongnaehyeok <cnh0301@gmail.com>
2020-11-03 14:13:49 +0900

    refactor: svg 파일 따로 분리

---
+++ src/assets/svg/Hamburger_Icon.js
@@ -0,0 +1,22 @@
+import React from "react";
+export default () => (
+  <svg
+    viewBox="0 0 32 32"
+    focusable="false"
+    style={{
+      fill: "none",
+      height: "16px",
+      width: "16px",
+      stroke: "#222222",
+      strokeWidth: "3",
+      overflow: "visible",
+      marginRight: "12px",
+    }}
+  >
+    <g fill="none" fillRule="nonzero">
+      <path d="m2 16h28"></path>
+      <path d="m2 24h28"></path>
+      <path d="m2 8h28"></path>
+    </g>
+  </svg>
+);
