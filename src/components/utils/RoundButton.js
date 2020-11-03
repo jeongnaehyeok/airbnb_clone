e8e32841356d562f403b1ef61ed82f00d9312350
jeongnaehyeok <cnh0301@gmail.com>
2020-11-03 14:17:25 +0900

    refactor: RoundButton switch로 변경

--- src/components/utils/RoundButton.js
+++ src/components/utils/RoundButton.js
@@ -4,21 +4,23 @@ import classNames from "classnames";
 import "./RoundButton.scss";
 
 const RoundButton = ({ navAtcive, type, contents, url, color, onClick }) => {
-  if (type == "button") {
-    return (
-      <button
-        onClick={onClick}
-        className={classNames("roundbutton", navAtcive, color)}
-      >
-        <div className="roundbutton--text">{contents}</div>
-      </button>
-    );
-  } else if (type == "a") {
-    return (
-      <a className={classNames("roundbutton", navAtcive, color)} href={url}>
-        <div className="roundbutton--text">{contents}</div>
-      </a>
-    );
+  switch (type) {
+    case "button":
+      return (
+        <button
+          onClick={onClick}
+          className={classNames("roundbutton", navAtcive, color)}
+        >
+          <div className="roundbutton--text">{contents}</div>
+        </button>
+      );
+    case "a": {
+      return (
+        <a className={classNames("roundbutton", navAtcive, color)} href={url}>
+          <div className="roundbutton--text">{contents}</div>
+        </a>
+      );
+    }
   }
 };
 
